d3f0a8ca017d28526b82269bb8f3a7e269c33aaf
Maxim Bogdanov <126229962+dtfyu3@users.noreply.github.com>
2025-05-22 07:53:46 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -51,7 +51,8 @@ function verifySignature(body, signature, headers) {
 
 // Отправка в Telegram
 async function sendTelegramAlert(title, vodUrl, streamerName) {
-  const message = `_${streamerName}_ только что завершил стрим _${title}_.  
+  const message = `_${streamerName}_ только что завершил стрим  
+_${title}_.  
 [Смотреть](${vodUrl})`;
   await axios.post(
     `https://api.telegram.org/bot${config.telegram.botToken}/sendMessage`,
